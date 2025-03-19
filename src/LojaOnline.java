baf23ae5f74f89054dcfd003ffbb424209572ed4
isgust <rocha.gustavops@gmail.com>
2025-03-19 01:05:52 -0300

    adição de logica de desconto na main

--- src/LojaOnline.java
+++ src/LojaOnline.java
@@ -2,20 +2,20 @@ import java.util.ArrayList;
 
 public class LojaOnline {
     public static void main(String[] args) {
-        // Creating Book objects
+        // Criando objetos Livro
         Livro livro1 = new Livro("Dom Quixote", 35.0, "Aventuras de um cavaleiro", "Miguel de Cervantes", "Aventura");
         Livro livro2 = new Livro("1984", 25.0, "Distopia política", "George Orwell", "Ficção Científica");
         Livro livro3 = new Livro("A Arte da Guerra", 150.0, "Estratégias de guerra", "Sun Tzu", "Estratégia");
 
-        // Creating Electronic objects
+        // Criando objetos Eletronico
         Eletronico eletronico1 = new Eletronico("Smartphone", 1200.0, "Celular de última geração", "Samsung", "2023");
         Eletronico eletronico2 = new Eletronico("Notebook", 2500.0, "Laptop para trabalho e jogos", "Dell", "2022");
         Eletronico eletronico3 = new Eletronico("Tablet", 800.0, "Dispositivo para leitura e entretenimento", "Apple", "2023");
 
-        // Creating ArrayList of Products
+        // Criando ArrayList de Produtos
         ArrayList<Produto> produtos = new ArrayList<>();
 
-        // Adding products to ArrayList
+        // Adicionando produtos ao ArrayList
         produtos.add(livro1);
         produtos.add(livro2);
         produtos.add(livro3);
@@ -23,7 +23,16 @@ public class LojaOnline {
         produtos.add(eletronico2);
         produtos.add(eletronico3);
 
-        // Displaying product details
+        // Criando ArrayList para simular o carrinho de compras
+        ArrayList<Produto> carrinho = new ArrayList<>();
+
+        // Adicionando produtos ao carrinho (simulação de compra)
+        carrinho.add(livro1);
+        carrinho.add(eletronico1);
+        carrinho.add(livro3);
+
+        // Exibindo detalhes dos produtos
+        System.out.println("Detalhes dos Produtos:");
         for (Produto produto : produtos) {
             System.out.println("Nome: " + produto.getNome());
             System.out.println("Preço: R$" + produto.getPreco());
@@ -42,5 +51,12 @@ public class LojaOnline {
             System.out.println("Desconto: R$" + produto.calcularDesconto());
             System.out.println("--------------------");
         }
+
+        // Calculando e exibindo o desconto total do carrinho
+        double descontoTotal = 0;
+        for (Produto produto : carrinho) {
+            descontoTotal += produto.calcularDesconto();
+        }
+        System.out.println("Desconto Total do Carrinho: R$" + descontoTotal);
     }
 }
